4423b9adf672a5fab46ca19ae9154abe3ace5275
Loïc Torres <loic.torresg@gmail.com>
2021-12-19 15:48:56 +0100

    stylizing

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import  './helpers/_variables.scss';
 
 // start the Stimulus application
 import './bootstrap';
